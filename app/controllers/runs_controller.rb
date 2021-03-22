cde2cdbed1527e6091c63378abf098b131bdeba2
Cameron Bisca <cjbisca@gmail.com>
2021-03-22 11:56:55 -0400

    Index, create, destroy actions in Runs controller

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,2 +1,32 @@
 class RunsController < ApplicationController
+
+    def index 
+        runs = Run.all
+        
+        render json: runs
+    end
+
+    def new 
+        run = Run.new
+    end
+
+    def create 
+        run = Run.create(run_params)
+        render json: user_cartoon
+
+    end
+
+    def destroy
+        run_to_delete = Run.find(params[:id])
+        run_to_delete.destroy
+
+        render json: run_to_delete
+    end
+
+    private
+
+    def run_params
+        params.permit(:id, :name, :user_id, :court_id)
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :runs
+  # resources :runs
   # resources :courts
   resources :users
 
   get '/courts', to: 'courts#index'
   get '/courts/:id', to: 'courts#show'
+
+  get '/runs', to: 'runs#index'
+  get '/runs/:id', to: 'runs#show'
 
 end
