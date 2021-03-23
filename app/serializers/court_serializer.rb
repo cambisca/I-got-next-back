9bc4c43c2e4a1b57f71f510bac75c06668d39830
Cameron Bisca <cjbisca@gmail.com>
2021-03-23 16:29:59 -0400

    nav bar restructuring

--- app/serializers/court_serializer.rb
+++ app/serializers/court_serializer.rb
@@ -1,5 +1,5 @@
 class CourtSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :borough, :zip_code, :condition, :latitude, :longitude, :trains, :img_url
 
-  has_many :runs 
+  has_many :users, through: :runs
 end
